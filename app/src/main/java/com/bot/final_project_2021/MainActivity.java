000cd7417995d25ee25b142cadd8fd27e44fccf4
IBOTIAndy <36949276+IBOTIAndy@users.noreply.github.com>
2021-06-13 15:15:02 +0800

    add moneyLayout

--- app/src/main/java/com/bot/final_project_2021/MainActivity.java
+++ app/src/main/java/com/bot/final_project_2021/MainActivity.java
@@ -2,7 +2,9 @@ package com.bot.final_project_2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,9 +23,18 @@ public class MainActivity extends AppCompatActivity {
     public void toAddData(View view){
         Toast.makeText(this, "It can go to add data.", Toast.LENGTH_SHORT).show();
     }
+
     public void toHistory(View view){
-        Toast.makeText(this, "It can go to History.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, moneyLayout.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+        else{
+            Log.d("Intent", "Can't go to history.");
+        }
+//        Toast.makeText(this, "It can go to History.", Toast.LENGTH_SHORT).show();
     }
+
     public void toSettings(View view){
         Toast.makeText(this, "It can go to Settings.", Toast.LENGTH_SHORT).show();
     }
---
+++ app/src/main/java/com/bot/final_project_2021/moneyLayout.java
@@ -0,0 +1,14 @@
+package com.bot.final_project_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class moneyLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_layout);
+    }
+}
